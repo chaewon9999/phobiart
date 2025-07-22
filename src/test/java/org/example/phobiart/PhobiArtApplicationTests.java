e03bc733dd75aeec0e00cdedecd537a5c1ec44a1
chaewon9999 <w0729@naver.com>
2025-07-22 14:42:37 +0900

    first commit to create git

---
+++ src/test/java/org/example/phobiart/PhobiArtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.phobiart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhobiArtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
